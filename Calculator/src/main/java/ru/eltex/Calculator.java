f28621af85bf1226e2c2ad95efc2658bc70729e8
Uvarov Vladimir <uvarov_v_s@mail.ru>
2019-07-03 16:44:27 +0700

    Реализация калькулятора

---
+++ Calculator/src/main/java/ru/eltex/Calculator.java
@@ -0,0 +1,36 @@
+package ru.eltex;
+import java.util.Scanner;
+public class Calculator {
+    public static void main (String[] args) {
+        System.out.println("Введите выражение в строку:");
+        Scanner reader = new Scanner(System.in);
+        String s = reader.nextLine();
+        String[] tmpStr = s.split(" ");
+        Double a =  0.0 ;
+        Double b =  0.0 ;
+        Double c =  0.0 ;
+        if (tmpStr.length>2) {
+            a =  Double.valueOf(tmpStr[0]) ;
+            b =  Double.valueOf(tmpStr[2]) ;
+            switch (tmpStr[1]) {
+                case "+":
+                    c = a + b;
+                    break;
+                case "-":
+                    c = a - b;
+                    break;
+                case "*":
+                    c = a * b;
+                    break;
+                case "/":
+                    c = a / b;
+                    break;
+                default:
+                    System.out.println("Введена неправильная команда");
+            }
+        }
+        else
+            System.out.println("Введено не полное выражение" );
+        System.out.println("Результат" + c);
+    }
+}
